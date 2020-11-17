9e5e6371464b88f6d91237f76c50bb43a298992e
schuelermine <git@anselmschueler.com>
2020-11-17 23:37:21 +0100

    Content

---
+++ DuplexDiv.js
@@ -0,0 +1,58 @@
+class DuplexDiv extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const α = this.attachShadow({mode: "open"});
+        
+        const ν = document.createElement("div");
+        const μ = document.createElement("div");
+        const τ = document.createElement("slot");
+        Object.defineProperty(this, "slotElement", {value: τ, writable: false});
+        Object.defineProperty(this, "divElement1", {value: ν, writable: false});
+        Object.defineProperty(this, "divElement2", {value: μ, writable: false});
+        μ.setAttribute("part", "duplicate");
+        μ.append(τ);
+        α.append(ν, μ);
+
+        const λ = (_, ω) => {this.render();}
+        this.observer = new MutationObserver(λ);
+        this.observer.observe(this, {subtree: true, childList: true, attributes: true})
+
+        const ξ = () => {
+            window.removeEventListener("DOMContentLoaded", ξ);
+            this.render();
+        }
+        window.addEventListener("DOMContentLoaded", ξ);
+
+        console.log("DuplexDiv: connected");
+    }
+
+    disconnectedCallback() {
+        console.log("DuplexDiv: disconnected");
+    }
+
+    static get observedAttributes() {
+        return [];
+    }
+
+    attributeChangedCallback(_name, _oldvalue, _newvalue) {
+        // pass
+    }
+
+    adoptedCallback() {
+        console.log("DuplexDiv: adopted");
+    }
+
+    render() {
+        while (this.divElement1.firstChild) {
+            this.divElement1.removeChild(this.divElement1.lastChild);
+        }
+        this.slotElement.assignedNodes().forEach(ω => {
+            this.divElement1.append(ω.cloneNode(true))
+        });
+    }
+}
+
+customElements.define("duplex-div", DuplexDiv)
---
+++ README.md
@@ -0,0 +1,11 @@
+# DuplexDiv
+`duplexDiv` is a custom HTML element that duplicates its contents.
+
+## Technologies used
+
+- Web Components
+  - Custom Elements
+  - Shadow DOM
+- Mutation Observers
+
+# ⚠ Requires a modern & updated browser. Uses newer JS syntax (arrow functions).
